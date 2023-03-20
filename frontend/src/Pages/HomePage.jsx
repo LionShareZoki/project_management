ff53fb1a44bf6500c1f13c51ea5dbee725c50e54
Zoran <pujic.zoran@gmail.com>
2023-03-20 18:36:00 +0100

    Implemented architecture

---
+++ frontend/src/Pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Registration from "../components/RegistrationForm";
+
+export default function HomePage() {
+  return (
+    <div>
+      <div>HomePage</div>
+      <div>
+        <Registration />
+      </div>
+    </div>
+  );
+}
